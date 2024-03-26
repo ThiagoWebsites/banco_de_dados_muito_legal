5ab4ff8a6bc8e5b441f8ce270fc7bee688d2444b
ThiagoWebsites <143205226+ThiagoWebsites@users.noreply.github.com>
2024-03-26 18:24:49 -0300

    Add files via upload

---
+++ parte1.sql.sql
@@ -0,0 +1,38 @@
+CREATE DATABASE sistema_vendas_22c;
+
+USE sistema_vendas_22c;
+
+CREATE TABLE cliente(
+nome varchar(50) not null,
+endereco varchar(100) not null,
+email varchar(50),
+celular tinyint(30) unsigned not null,
+dt_nascimento date,
+d
+);
+
+CREATE TABLE produto(
+id_produtos int not null auto_increment,
+nome varchar(50) not null,
+preco decimal (6,2) not null,
+descricao varchar(50) not null,
+quantidade_estoque tinyint not null,
+PRIMARY KEY(id_produtos)
+);
+
+CREATE TABLE pedido(
+id_pedido tinyint unsigned primary key auto_increment not null,
+dt_compra date,
+valor decimal(6,2),
+dt_estimada_entrega date,
+foreign key(id_produtos)
+REFERENCES produto(id_produtos)
+);
+
+CREATE TABLE itens(
+quantidade tinyint unsigned not null,
+valor_unitário decimal(6,2),
+foreign key(numero_pedido)
+references pedido(numero_pedido),
+valor_total decimal(6,2)
+);
