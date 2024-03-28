c0f386157b77220b8f39ecfa808180d7721f9a31
ThiagoWebsites <143205226+ThiagoWebsites@users.noreply.github.com>
2024-03-28 13:43:31 -0300

    Add files via upload

---
+++ parte2.sql
@@ -0,0 +1,12 @@
+USE sistema_vendas_22c;
+
+INSERT into client ( nome, endereco, email, celular, dt_nascimento) 
+VALUES
+( 'thiago', 'Rua das Belezas', 'thiago@gmail.com', 11984735472, '1980-09-15'),
+( 'Gabriel', 'Rua das Belezas', 'thiago@gmail.com', 11984735472, '1980-09-15'),
+( 'Mateus', 'Rua das Belezas', 'thiago@gmail.com', 11984735472, '1980-09-15'),
+( 'thiago', 'Rua das Belezas', 'thiago@gmail.com', 11984735472, '1980-09-15'),
+( 'thiago', 'Rua das Belezas', 'thiago@gmail.com', 11984735472, '1980-09-15'),
+( 'thiago', 'Rua das Belezas', 'thiago@gmail.com', 11984735472, '1980-09-15'),
+( 'thiago', 'Rua das Belezas', 'thiago@gmail.com', 11984735472, '1980-09-15'),
+( 'thiago', 'Rua das Belezas', 'thiago@gmail.com', 11984735472, '1980-09-15');
